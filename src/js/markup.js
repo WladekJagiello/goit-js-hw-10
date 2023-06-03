bd6615ba1b73c8275c24f3e40b42bcf39316b7de
Vadym <120068188+WladekJagiello@users.noreply.github.com>
2023-06-03 12:46:52 +0300

    ...

--- src/js/markup.js
+++ src/js/markup.js
@@ -46,7 +46,7 @@ function createMarkup(data) {
 }
 
 function createCardInfo(data) {
-  infoEl.innerHTML = `<img src="${data.url}" width="100%"><div style="padding: 15px; padding-top: 0;"><h1>${data.breeds.name}</h1><p>${data.breeds.description}</p><b>Temperament: </b><span> ${data.breeds.temperament}</span></div>`;
+  infoEl.innerHTML = `<img src="${data.url}" width="100%"><div style="padding: 15px; padding-top: 0;"><h1>${data.breeds[0].name}</h1><p>${data.breeds[0].description}</p><b>Temperament: </b><span> ${data.breeds[0].temperament}</span></div>`;
   infoEl.style.cssText = `width: 350px; margin-top: 75px; margin-left: auto; margin-right: auto; border: 2mm ridge rgba(211, 211, 211, .5); background-color: #fff;`;
   loaderEl.style.opacity = '0';
   backgroundEl.style.opacity = '1';
